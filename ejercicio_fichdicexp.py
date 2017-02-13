7bbbee31651279831663c3c680a6b2f226774842
Borja Egea Madrid <boegma@hotmail.com>
2017-02-13 01:57:29 +0100

    He hecho el ejercicio 13.5: Ficheros, diccionarios y excepciones.

--- ejercicio_fichdicexp.py
+++ ejercicio_fichdicexp.py
@@ -16,11 +16,11 @@ nombre_usu1 = 'root'
 nombre_usu2 = 'inventado'
 
 nombre_shell1 = diccionario_usuarios[nombre_usu1]
-print "Usuario:" + nombre_usu1, " Shell:" + nombre_shell1[0:-1]
+print ("Usuario:" + nombre_usu1, " Shell:" + nombre_shell1[0:-1])
 
 try:
 	nombre_shell2 = diccionario_usuarios[nombre_usu2]
-	print "Usuario:", nombre_usu2, " Shell:", nombre_shell2[0:-1]
+	print ("Usuario:", nombre_usu2, " Shell:", nombre_shell2[0:-1])
 
 except KeyError:
-	print "No se ha podido encontrar el usuario: " + "'" + nombre_usu2 + "'"
+	print ("No se ha podido encontrar el usuario: " + "'" + nombre_usu2 + "'")
